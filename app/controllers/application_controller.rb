560118fb4d0810d4219c9c82b7e7eb0ec1e45e83
Heng Desmond <desmond@dsrecruitment.sg>
2017-03-31 13:02:46 +0800

    added new fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :nric, :contact_number, :gender, :date_of_birth, :address, :student_status, :student_school, :bank_name, :bank_account_number, :colored_hair_status, :tattoo_status])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :nric, :contact_number, :gender, :date_of_birth, :address, :student_status, :student_school, :bank_name, :bank_account_number, :colored_hair_status, :tattoo_status])
+  end
 end
